df2782d1af38b3f0aae2cbe56d04935c8f9ba130
paxpo <akdzktp1@naver.com>
2018-03-30 02:23:36 +0800

    1. 정규분포 함수 변수 이름 수정했습니다. Variance -> Standard_deviation 2. 표준정규분포표 함수를 만들었습니다. 3. 표준정규분포 특정구간 함수를 만들었습니다.
    ps. 모든 함수는 소수점 4자리 값까지 리턴하며, 소수점 5번째 자리가 5보다 크거나 같을 시 반올림이 됩니다.

--- func/ND_pdf.py
+++ func/ND_pdf.py
@@ -6,4 +6,4 @@ from sympy import exp,sqrt,pi,Integral,Symbol
 def Normald_pdf(Mean,Standard_deviation,Pmin,Pmax):                       #평균,분산,구간1,구간2
     x = Symbol('x')
     f = exp(-(x-Mean)**2/(2*Standard_deviation**2))/(Standard_deviation*sqrt(2*pi))
-    print(Integral(f, (x,Pmin,Pmax)).evalf())
+    return Integral(f, (x,Pmin,Pmax)).evalf(4)
