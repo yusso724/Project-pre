22585d946949c0e7b2416eadbe766281721c8319
paxpo <akdzktp1@naver.com>
2018-03-30 01:12:16 +0800

    정규분포 함수 변수 이름 수정했습니다 Variance -> Standard_deviation 표준정규분포 함수 만들었습니다. 추후 Z test 진행 시 표 안보고 바로 함수로 알 수 있게 만들어 봤습니다.
    ps. 상기 함수들은 결과값을 리턴하지 않고, 프린트 하게 해놓았습니다.
    ps2. Z값에 대응하는 값은 소수점 4자리까지만 표시하였습니다.

--- func/Normal_distribution_pdf.py
+++ func/Normal_distribution_pdf.py
@@ -1,9 +1,9 @@
 #-*- coding: utf-8 -*-
 #by minseok
 #본 코드는 정규분포의 확률밀도함수를 구현한 함수입니다.
-from sympy import exp, sqrt, pi, Integral, Symbol
+from sympy import exp,sqrt,pi,Integral,Symbol
 
-def Normald_pdf(Mean,Variance,Pmin,Pmax):                       #평균,분산,구간1,구간2
+def Normald_pdf(Mean,Standard_deviation,Pmin,Pmax):                       #평균,분산,구간1,구간2
     x = Symbol('x')
-    f = exp(-(x-Mean)**2/(2*Variance**2))/(Variance*sqrt(2*pi))
+    f = exp(-(x-Mean)**2/(2*Standard_deviation**2))/(Standard_deviation*sqrt(2*pi))
     print(Integral(f, (x,Pmin,Pmax)).doit().evalf())
---
+++ func/Standard_df.py
@@ -0,0 +1,10 @@
+#-*- coding: utf-8 -*-
+#by minseok
+#본 코드는 표준정규분포표 함수입니다.
+from sympy import exp,sqrt,pi,Integral,Symbol
+
+def Standard_nd(Z_value):                                           #표준정규분포에서 분산은 1, 평균은 0입니다.
+    x = Symbol('x')
+    Z = x
+    f = exp(-(Z)**2/2)/(sqrt(2*pi))
+    print("%0.4f"%float(Integral(f,(Z,0,Z_value)).doit().evalf()))
