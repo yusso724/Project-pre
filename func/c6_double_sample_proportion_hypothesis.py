b5f87ad239c4b1b796343ba8464fe0d7543819a2
junholeephy <skyblue1293@naver.com>
2018-04-10 15:29:37 +0800

    proportion hypothesis test with statsmodels

--- func/c6_double_sample_proportion_hypothesis.py
+++ func/c6_double_sample_proportion_hypothesis.py
@@ -4,18 +4,23 @@ from matplotlib import pyplot as plt
 import numpy as np
 import math
 import sys,os
+import scipy
+from statsmodels.stats import proportion
 from sympy import exp,sqrt,pi,Integral
 sys.path.append("/Users/leejunho/Desktop/git/python3Env/group_study/project_pre/func/")
 from c1_basic_statistic import *
 
 
-
 def bothSide_hypothesis(proportion1=0.1,event_num1=100,proportion2=0.5,event_num2=100):
+    # this is Z test for proportion test
+    # returns [self calculated Zc, Zc from statsmodels, p-value from statsmodels corresponding Zc]
     # H0 : P1 = P2
     # H1 : P1 != P2
     pbar = (event_num1*proportion1+event_num2*proportion2)/(event_num1+event_num2)
     Zc = (proportion1-proportion2) / math.sqrt(pbar*(1-pbar)/event_num1 + pbar*(1-pbar)/event_num2)
-    return Zc
+    Ztest = proportion.proportions_ztest([int(event_num1*proportion1), int(event_num2*proportion2)],[event_num1,event_num2],0)
+    LIST = [Zc, Ztest[0], Ztest[1]]
+    return LIST
 
 
 
