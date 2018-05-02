d795bb03d7523a12edcddd40448a4f22a31c873e
manggny <manggny@126.com>
2018-05-02 12:25:07 +0800

    scatter with python! _ modified

--- data_txt/BEIJING_Aqi/man_pyscat.py
+++ data_txt/BEIJING_Aqi/man_pyscat.py
@@ -45,19 +45,24 @@ def man_py_scatter(filename):
             y_list_int.append(float(y_list[k]))
 
     #print(len(y_list_int))
-        for j in range(i,x_len):
+        plt.figure(10)
+        for j in range(1,x_len):
             x_list_int = []
             x_list = []
             x_list = list_c[j]
             x_label = x_list[0]
             for k in range(1,len(x_list)):
                 x_list_int.append(float(x_list[k]))
+            num = 330+j
+            a = plt.subplot(num)
             plt.scatter(x_list_int, y_list_int, s=size, c='black', marker='o', alpha=0.5, label='C1')
-            plt.title(y_label+'_'+'basic scatter plot ')
-            plt.xlabel(x_label)
-            plt.ylabel(y_label)
-            plt.savefig(x_label+'_'+y_label+'.pdf')
-            plt.close('all')
+
+            #plt.xlabel(x_label)
+            plt.text(0.5,0.9,x_label, ha='center', va='center',transform = a.transAxes)
+            #plt.ylabel(y_label)
+        #plt.title('y = '+y_label+'_'+'x_others')
+        plt.savefig(y_label+'_'+'others'+'.pdf')
+        plt.close('all')
   #      x_list_int.clear()
    #     x_list.clear()
    # y_list_int.clear()
