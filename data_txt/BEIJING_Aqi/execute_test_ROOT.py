eade0350358c2c876af785724d41fa4b5d61dc5a
junholeephy <skyblue1293@naver.com>
2018-05-05 02:52:58 +0800

    new script

--- data_txt/BEIJING_Aqi/execute_test_ROOT.py
+++ data_txt/BEIJING_Aqi/execute_test_ROOT.py
@@ -4,23 +4,34 @@ gBenchmark.Start("All in One")
 import sys
 import os
 
+os.system("rm -rf ROOT_TGraph_basic")
+os.system("rm -rf ROOT_2D_colz")
+os.system("rm -rf ROOT_2D_surf3")
+os.system("rm -rf ROOT_2D_profileX_pols")
+os.system("rm -rf ROOT_files")
+
 INfile = "carbon_copied_data/Aqi_Beijing_day.txt"
-BIN_Num_2D = 20    # X bin number of 2D
+BIN_Num_2D = 5    # X bin number of 2D
 YBIN_Num_2D = 20   # Y bin number of 2D
 oneD_NBins = 100
 
 #### option to draw ###
-TGraph_2d_basic = 1 ;   Marker_style = ",1"
-TH1D_transfer = 0
-TH2D_transfer = 0
-TH2D_colz_surf3 = 0
-TH2D_profileX = 0
+N_sigma = 5    ## Skiming of txt outof specific sigma region
+TGraph_2d_basic = 1 ;   Marker_style = ",1";   poly19_fit =",0"
+TH1D_transfer = 1
+TH2D_transfer = 1
+if TH2D_transfer==1:
+    TH2D_colz_surf3 = 1
+    TH2D_profileX = 1
 
 PYTHON_HIST = 0  # Do not change, this need to be set as python3
 
 sys.path.append("/Users/leejunho/Desktop/git/python3Env/group_study/project_pre/func")
 from d1_remake_txt import MakeTXT
-Infile = MakeTXT(INfile)
+from d0_Nsigma_skiming import N_sigma_skimming
+INFILE = MakeTXT(INfile)
+Infile = N_sigma_skimming(INFILE, N_sigma)
+
 
 sys.path.append("/Users/leejunho/Desktop/git/python3Env/group_study/fruit_team/ROOT/Project/functions/rawTxt_Tree_root")
 from Raw_text_to_Tree_root import Raw_text_to_Tree_root
@@ -46,8 +57,9 @@ from Tree_to_D1H_CutnGenerate_BEIJING_AIR import REGENERATE_TREE_WITH_CUT
 NEW_Tree_PATH = REGENERATE_TREE_WITH_CUT(To_Tree,".")
 
 if TGraph_2d_basic==1:
-    TGraph_2D_basic = "root -l -q /Users/leejunho/Desktop/git/python3Env/group_study/fruit_team/ROOT/Project/functions/TGraph_saver/Tree_branches_to_vector.C\("+"'"+'"'+NEW_Tree_PATH+'"'+"'"+Marker_style+"\)"
+    TGraph_2D_basic = "root -l -q /Users/leejunho/Desktop/git/python3Env/group_study/fruit_team/ROOT/Project/functions/TGraph_saver/Tree_branches_to_vector.C\("+"'"+'"'+NEW_Tree_PATH+'"'+"'"+Marker_style+poly19_fit+"\)"
     os.system(TGraph_2D_basic)
+#    os.system("rm -rf ROOT_TGraph_basic")
     os.system("mkdir ROOT_TGraph_basic")
     os.system("mv *basic_*.pdf ROOT_TGraph_basic")
 
@@ -65,6 +77,8 @@ if TH2D_transfer==1:
         twoD_plot_save = "root -l -q /Users/leejunho/Desktop/git/python3Env/group_study/fruit_team/ROOT/Project/functions/2Dplots_Saver/TwoD_Plot_Saver_default.C\("+"'"+'"'+HistROOT_PATH_2D+'"'+"'"+"\)"
         os.system(twoD_plot_save)
         #os.system("mkdir ROOT_2D_defalut")
+#        os.system("rm -rf ROOT_2D_colz")
+#        os.system("rm -rf ROOT_2D_surf3")
         os.system("mkdir ROOT_2D_colz")
         os.system("mkdir ROOT_2D_surf3")
         #os.system("mv *defalut_2D.pdf ROOT_2D_defalut")
@@ -74,6 +88,7 @@ if TH2D_transfer==1:
     if TH2D_profileX==1:
         twoD_profile_pol_save = "root -l -q /Users/leejunho/Desktop/git/python3Env/group_study/fruit_team/ROOT/Project/functions/2Dplots_Saver/TwoD_profileX_pol_fitter.C\("+"'"+'"'+HistROOT_PATH_2D+'"'+"'"+"\)"
         os.system(twoD_profile_pol_save)
+#        os.system("rm -rf ROOT_2D_profileX_pols")
         os.system("mkdir ROOT_2D_profileX_pols")
         os.system("mv *profileX_pol_2D.pdf ROOT_2D_profileX_pols")
 
@@ -81,6 +96,7 @@ if TH2D_transfer==1:
 #os.system("mv *.root ROOT_files") 
 
 if PYTHON_HIST != 1:
+#    os.system("rm -rf ROOT_files")
     os.system("mkdir ROOT_files")
     os.system("mv *.root ROOT_files")
     gBenchmark.Show("All in One")
@@ -119,17 +135,21 @@ elif (PYTHON_HIST==1) & (TH1D_transfer==1):
         t_distribution(TXT_FILE_LIST[ij],TXT_FILE_LIST_largeBin[ij], Show_T=True, Show_sample=True, Show_Gaus=True)
         Sample_Variance(TXT_FILE_LIST[ij], TXT_FILE_LIST_largeBin[ij])
 
+    os.system("rm -rf ROOT_python_plots")
+    os.system("rm -rf ROOT_python_hist_texts")
     os.system("mkdir ROOT_python_plots")
     os.system("mkdir ROOT_python_hist_texts")
     os.system("mv *py.pdf ROOT_python_plots")
     os.system("mv *hist.txt ROOT_python_hist_texts")
     os.system("mv *hist_largeBin.txt ROOT_python_hist_texts")
 
+#    os.system("rm -rf ROOT_files")
     os.system("mkdir ROOT_files")
     os.system("mv *.root ROOT_files")
     gBenchmark.Show("All in One")
 
 else:
+#    os.system("rm -rf ROOT_files")
     os.system("mkdir ROOT_files")
     os.system("mv *.root ROOT_files")
     gBenchmark.Show("All in One")
