e058bcfa0b105ab8323d377e6272dcbeea836fd0
manggny <manggny@126.com>
2018-05-06 13:18:39 +0800

    cut해서 *_cutted.txt파일로 바꾸는 함수입니다! 저장 위치는 txt데이터 받아온곳과 동일합니다. 사용방법은 : 시작하면 각 원소의 넘버가 나오고 어느 원소를 컷할껀지 선택하는 창이 나옵니다. 선택하면 최소값과 최대값을 선택하는 창이 나옵니다 (원하지 않으면 '-'입력) 컷트가 끝난뒤 더 커트가 필요한지 물어봅니다. (필요 없으면 '-'입력) . - 이 입력되면 프로그램을 마치고 커팅된 파일을 저장합니다.

---
+++ func/txt_cut_apply.py
@@ -0,0 +1,104 @@
+import sys
+import os
+from d0_makelist_column import *
+from d0_makelist import *
+import numpy as np
+
+def cut_apply(filename):
+    path = '../data_txt/BEIJING_Aqi'
+    if (filename[0] == "/"):
+        filename = filename
+    elif ((filename[0] == "C") & (filename[1] == ":")):
+        filename = filename
+    else:
+        filename = os.getcwd() + "/" + filename  # get the path included filename
+    loca = len(filename)
+    for i in range(1, len(filename) + 1):  # find the "/" location
+        if (filename[-i] == "/"):
+            loca = i - 1
+            break
+
+    FILENAME = filename.replace(filename[:-loca], "")  # this is the shorten filename
+    filename_No_Txt = FILENAME.replace(".txt", "")
+    infile = filename
+
+    list_c = MakeList_column(infile)
+    list_a = MakeList(infile)
+    elements = list_a[0]
+    ele_name = "Elements :"
+    for i in range(len(elements)):
+        ele_name += elements[i]+"="+str(i)+" "
+    print(ele_name)
+    x_list = []
+    del_list = []
+    for i in range(len(list_a)):
+        del_list.append(0)
+    #print(len(del_list))
+    while 1:
+
+        index = input("what element do you want to cut?(if you don't want cut the raw data, please enter '-')")
+       # print(index)
+        if index =='-':
+            print("end!")
+            break
+           # print("wrong input!,please select number 0~9")
+        x_list = list_c[int(index)]
+        min = input("input min-value of cut range (if you don't want to set the cutting min-value, please enter '-')")
+        try:
+            min = float(min)
+        except:
+            min = -100000
+        max = input("input max-value of cut range (if you don't want to set the cutting max-value, please enter '-')")
+        try:
+            max = float(max)
+        except:
+            max = 100000
+#        del(list_a[0])
+ #       print(list_a)
+       # print(len(list_a))
+        j = 1
+
+        while 1:
+            if j >= len(x_list):
+                break
+            if float(x_list[j])<= float(min) or float(x_list[j]) >= float(max):
+                #print(j)
+                del_list[j] = 1
+                j +=1
+             #   del(x_list[j])
+              #  del(list_a[j])
+            else:
+                j += 1
+    #    print("error！")
+
+    #print(len(del_list),len(list_a))
+    i = 1
+    while i<len(del_list):
+        if del_list[i] == 1:
+            #print(i)
+            del(list_a[i])
+            del(del_list[i])
+        else:
+            i+=1
+    FN = filename.replace(".txt", "_cutted.txt")
+   # FN = path + FN
+   # print(FN)
+    Of = open(FN, "w+")
+    for i in range(len(list_a)):
+       Of.write(str(list_a[i])+" \n")
+    Of.close()
+   # os.system("move *_cutted.pdf "+ path)
+    return FN
+
+
+    #print(type(index),type(min),max)
+
+def main():
+    file1 = "../data_txt/BEIJING_Aqi/Aqi_Beijing_Holi.txt"
+    path =cut_apply(file1)
+    print(path)
+    # print("over!",i,j,x,x_len)
+
+if __name__ == "__main__":
+    main()
+
