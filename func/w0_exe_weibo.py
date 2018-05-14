79f3240a167999eb33b702d645586f638f204939
junholeephy <skyblue1293@naver.com>
2018-05-15 01:11:24 +0800

    exe weibo

--- func/w0_exe_weibo.py
+++ func/w0_exe_weibo.py
@@ -3,16 +3,16 @@ import time
 sys.path.append("/Users/leejunho/Desktop/git/python3Env/group_study/project_pre/func")
 from w0_WEIBO import WEIBO
 
-ID_junho = ""   # "skyblue@126.com"
-PASSWD_junho = "" # "ttttttt"
+ID_junho = ""
+PASSWD_junho = ""
 
 weibo = WEIBO()
-weibo.AWAKE_BROWSER(filename = "PM_test.txt")
+weibo.AWAKE_BROWSER(filename = "PM_20180401_test.txt")
 weibo.LOGIN(ID=ID_junho,PASSWD=PASSWD_junho)
 
-year = 2014
-day = 14
-month = 12
+year = 2018
+day = 31
+month = 3
 for i in range(30):
     INFO =  weibo.DATE_MAKER(year,month,day)
     for j in range(18):
@@ -25,12 +25,15 @@ for i in range(30):
         for PG in range(50):
             web_page = weibo.MAKE_WEIBO_URL(INFO,LOCAL_NUM = LOC_NUM ,PAGE=PG+1); #print(web_page)
             CONTI = weibo.ACCESS_URL(web_page)
-            print "!!!!!Current page keyword number : ",weibo.COUNT
+            print "!!!!! Until Current page, cumulated keyword number : ",weibo.COUNT
+            print "!!!!! District Number : ", LOC_NUM
+            print "!!!!! Page Number : ", PG+1
+            print ""; print ""
             if(CONTI == 0):
                 break
     print " * TOTAL COUNT NUM OF PM WORD : ",weibo.COUNT
-    INFO_STR = MAKE_DATE_STR(INFO)
-    INPUT_LIST = [INFO_STR[0], weibo.COUNT]
+    INFO_STR = weibo.MAKE_DATE_STR(INFO)
+    INPUT_LIST = [INFO_STR[3], weibo.COUNT]
     weibo.CREATE_n_WRITE_INTO_TXT(INPUT_LIST)
     weibo.GERP_WORD_to_zero()
     month = INFO[1]
